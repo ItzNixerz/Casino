c03143820f7cddcb2b7b15c3de7f641377d52e58
ItzNixerz <nick7topala@gmail.com>
2025-02-06 22:14:17 -0500

    Update blackjack.js

--- Games/blackjack.js
+++ Games/blackjack.js
@@ -1,15 +1,20 @@
-const cardImages = {
-    '2': 'https://th.bing.com/th/id/OIP.NHh8Im-4L6PrzVPlPcaJegHaKu?rs=1&pid=ImgDetMain', // 2 of Spades
-    '3': 'https://th.bing.com/th/id/OIP.HJdjDGiW9TawXz3YGo0hUgHaKv?rs=1&pid=ImgDetMain', // 3 of Spades
-    '4': 'https://thumbs.dreamstime.com/b/four-spades-playing-card-isolated-white-four-spades-playing-card-isolated-white-clipping-path-included-172561254.jpg', // 4 of Spades
-    '5': 'https://th.bing.com/th/id/OIP.bFrUmdzDgAGviGaQWOELLwAAAA?rs=1&pid=ImgDetMain', // 5 of Spades
-    '6': 'https://th.bing.com/th/id/R.be3130febe457ee6889af4dfd2be2e4b?rik=etJiFyMLv9DQYw&riu=http%3a%2f%2fopenclipart.org%2fimage%2f800px%2fsvg_to_png%2f192487%2f1396450565.png&ehk=E1X%2bAAmreSzWRN5p8WDyZUUgDqAUgQNibvUclnZ8VEA%3d&risl=&pid=ImgRaw&r=0', // 6 of Spades
-    '7': 'https://th.bing.com/th/id/R.592459ef3a726a2dbb74a481e90c3a59?rik=%2fASuuRVRsy80wQ&riu=http%3a%2f%2forig02.deviantart.net%2fad55%2ff%2f2011%2f180%2f3%2fd%2f7_of_spades_by_farvei-d3kgfla.png&ehk=DBDU2INL9gngvqpbw1ODsWkm8YELZOR3ml0wMw%2bnBuc%3d&risl=&pid=ImgRaw&r=0', // 7 of Spades
-    '8': 'https://th.bing.com/th/id/R.59d7bc83c9f638f69001b2c218b5a0ec?rik=GHuYbeLe39mCmg&riu=http%3a%2f%2ffc05.deviantart.net%2ffs71%2ff%2f2011%2f180%2fd%2f6%2f8_of_spades_by_farvei-d3kgffc.png&ehk=brf71%2fjT2mcaBdppAywj9AxFXjd0UZ3gATo3XqmDZuY%3d&risl=&pid=ImgRaw&r=0', // 8 of Spades
-    '9': 'https://i.pinimg.com/736x/7d/47/a3/7d47a37c1bf84fdeb0a83137ca61d962.jpg', // 9 of Spades
-    '10': 'https://www.keen.com/wp-content/uploads/sites/2/2022/08/Jack-of-Spades-Meaning.jpg', // 10 of Spades
-    'J': 'https://www.keen.com/wp-content/uploads/sites/2/2022/08/Jack-of-Spades-Meaning.jpg', // Jack of Spades
-    'Q': 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4d364ce1-cddf-46f1-8a9d-c0e8cc9c36cc/dawrv2l-d5e735db-bea6-46e2-be6a-51c4e289b36b.png/v1/fill/w_1024,h_1364,q_80,strp/my_playing_cards_v2___queen_of_spades_by_gabe0530_dawrv2l-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM2NCIsInBhdGgiOiJcL2ZcLzRkMzY0Y2UxLWNkZGYtNDZmMS04YTlkLWMwZThjYzljMzZjY1wvZGF3cnYybC1kNWU3MzVkYi1iZWE2LTQ2ZTItYmU2YS01MWM0ZTI4OWIzNmIucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.pLihV6HdFGd320zaUUf2N5STrKNH7KHndBhcw4GJh1k', // Queen of Spades
-    'K': 'https://thumbs.dreamstime.com/b/large-index-playing-card-king-spades-american-deck-122573720.jpg', // King of Spades
-    'A': 'https://th.bing.com/th/id/R.6080b0e3696e34e43de2f73da1620e51?rik=%2bZ%2fesTFEWXHCfQ&riu=http%3a%2f%2fclipart-library.com%2fimages%2f8cAbMe7Ri.png&ehk=W8vndfEhLZdoJsb2oXUiAxwKLOl81Ah2Eo4QL9vA%2bjY%3d&risl=&pid=ImgRaw&r=0' // Ace of Spades
-};
+// Render cards in the player's or dealer's hand with images
+function renderCards(hand, isPlayer) {
+    const cardDeck = isPlayer ? document.getElementById('player-cards') : document.getElementById('dealer-cards');
+    cardDeck.innerHTML = '';  // Clear previous cards
+
+    hand.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        const cardValue = card.slice(0, -1); // Get the card value (e.g., 'A', '10', 'K')
+        const cardImage = cardImages[cardValue]; // Get the card image URL
+        const img = document.createElement('img');
+        img.src = cardImage;
+        img.alt = card;
+        img.style.width = '60px';  // Set image width
+        img.style.height = '90px';  // Set image height
+        cardDiv.appendChild(img);
+        cardDeck.appendChild(cardDiv);
+    });
+}
+
